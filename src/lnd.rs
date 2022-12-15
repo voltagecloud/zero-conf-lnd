78ce257ac77bb3fe84a1e890b6c6f265b48f06c0
Tony Giorgio <tony@voltage.cloud>
2022-12-15 14:45:16 -0600

    Working 0 conf acceptance

--- src/lnd.rs
+++ src/lnd.rs
@@ -1,8 +1,12 @@
 use crate::config::ChannelAcceptanceParams;
 use std::collections::HashMap;
+use tokio::sync::mpsc::Sender;
+use tonic_lnd::lnrpc::ChannelAcceptRequest;
 
 use crate::config::LndConfig;
 use secp256k1::PublicKey;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
 use tonic_lnd::lnrpc::ChannelAcceptResponse;
 use tonic_lnd::LightningClient;
 
@@ -35,8 +39,8 @@ pub(crate) async fn start_channel_acceptor(
             .or_insert(c.confs);
     });
 
-    let (tx, rx) = tokio::sync::mpsc::channel::<ChannelAcceptResponse>(1024);
-    let receive_stream = tokio_stream::wrappers::ReceiverStream::new(rx);
+    let (tx, rx) = mpsc::channel::<ChannelAcceptResponse>(1024);
+    let receive_stream = ReceiverStream::new(rx);
 
     let mut channel_acceptor = lnd_client
         .channel_acceptor(receive_stream)
@@ -51,20 +55,74 @@ pub(crate) async fn start_channel_acceptor(
         .await
         .expect("Failed to receive HTLCs")
     {
-        tracing::info!("received request to accept channel...");
+        tracing::debug!("received request to accept channel...");
 
         // check zero conf status
-        if !channel.wants_zero_conf {}
+        if !channel.wants_zero_conf {
+            // TODO check the conf amount if less than normal
+            tracing::debug!("accepting normal channel...");
+            accept(tx.clone(), channel, true, 0).await;
+            tracing::info!("normal channel accepted");
+            continue;
+        }
 
         let node_pubkey = PublicKey::from_slice(&channel.node_pubkey)
             .expect("node_pubkey should be a PublicKey")
             .to_string();
 
-        if let Some(whitelisted_channel) = whitelist.get(&node_pubkey) {
-            // TODO parse the confirmation minimum and return the channel
-            // acceptor response
+        if let Some(whitelisted_channel_confs) = whitelist.get(&node_pubkey) {
+            // parse the confirmation minimum and return the channel acceptor response
+            tracing::debug!("accepting zero conf channel...");
+            accept(tx.clone(), channel, true, *whitelisted_channel_confs as u32).await;
+            tracing::info!("zero conf channel accepted");
         } else {
-            // not in our whitelist, send default response
+            // not in our whitelist, send failure response
+            tracing::debug!("denying zero conf channel...");
+            deny(tx.clone(), channel).await;
+            tracing::info!("zero conf channel denied");
         }
     }
+}
+
+async fn accept(
+    tx: Sender<ChannelAcceptResponse>,
+    channel: ChannelAcceptRequest,
+    zero_conf: bool,
+    min_accept_depth: u32,
+) {
+    // Only the accept and id are important, ignore the rest
+    tx.send(ChannelAcceptResponse {
+        accept: true,
+        pending_chan_id: channel.pending_chan_id,
+        error: String::from(""),
+        upfront_shutdown: String::from(""),
+        csv_delay: 0,
+        reserve_sat: 0,
+        in_flight_max_msat: 0,
+        max_htlc_count: 0,
+        min_htlc_in: 0,
+        min_accept_depth,
+        zero_conf,
+    })
+    .await
+    .unwrap()
+}
+
+async fn deny(tx: Sender<ChannelAcceptResponse>, channel: ChannelAcceptRequest) {
+    // Only the accept and id are important, ignore the rest
+    tx.send(ChannelAcceptResponse {
+        accept: false,
+        pending_chan_id: channel.pending_chan_id,
+        error: String::from(""),
+        upfront_shutdown: String::from(""),
+        csv_delay: 0,
+        reserve_sat: 0,
+        in_flight_max_msat: 0,
+        max_htlc_count: 0,
+        min_htlc_in: 0,
+        min_accept_depth: 0,
+        zero_conf: true,
+    })
+    .await
+    .unwrap()
 }
