caba0ec5b7a3a7e81acebe1e866c7675f203250d
Tony Giorgio <tony@voltage.cloud>
2022-12-15 13:18:14 -0600

    Swap out versions of tonic_lnd to support 0 conf

--- src/lnd.rs
+++ src/lnd.rs
@@ -3,11 +3,12 @@ use std::collections::HashMap;
 
 use crate::config::LndConfig;
 use secp256k1::PublicKey;
-use tonic_openssl_lnd::lnrpc::ChannelAcceptResponse;
-use tonic_openssl_lnd::LndLightningClient;
+use tonic_lnd::lnrpc::ChannelAcceptResponse;
+use tonic_lnd::LightningClient;
 
-pub(crate) async fn create_client(cfg: LndConfig) -> LndLightningClient {
-    tonic_openssl_lnd::connect(
+pub(crate) async fn create_client(cfg: LndConfig) -> LightningClient {
+    tracing::info!("starting connection to lnd");
+    tonic_lnd::connect(
         cfg.host,
         cfg.port as u32,
         cfg.cert_location,
@@ -20,9 +21,11 @@ pub(crate) async fn create_client(cfg: LndConfig) -> LndLightningClient {
 }
 
 pub(crate) async fn start_channel_acceptor(
-    mut lnd_client: LndLightningClient,
+    mut lnd_client: LightningClient,
     whitelisted_channels: Vec<ChannelAcceptanceParams>,
 ) {
+    tracing::info!("starting channel acceptor");
+
     // convert vec to hashmap for easy access
     let mut whitelist: HashMap<String, u8> = HashMap::new();
     whitelisted_channels.into_iter().for_each(|c| {
@@ -41,11 +44,18 @@ pub(crate) async fn start_channel_acceptor(
         .expect("could not start channel acceptor")
         .into_inner();
 
+    tracing::info!("channel acceptor started");
+
     while let Some(channel) = channel_acceptor
         .message()
         .await
         .expect("Failed to receive HTLCs")
     {
+        tracing::info!("received request to accept channel...");
+
+        // check zero conf status
+        if !channel.wants_zero_conf {}
+
         let node_pubkey = PublicKey::from_slice(&channel.node_pubkey)
             .expect("node_pubkey should be a PublicKey")
             .to_string();
@@ -53,6 +63,8 @@ pub(crate) async fn start_channel_acceptor(
         if let Some(whitelisted_channel) = whitelist.get(&node_pubkey) {
             // TODO parse the confirmation minimum and return the channel
             // acceptor response
+        } else {
+            // not in our whitelist, send default response
         }
     }
 }
